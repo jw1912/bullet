3382ddaa1bd52472f2d6d52338f7b6fde9b178c3
JacquesRW <jamie.whiting2003@icloud.com>
2023-09-01 14:00:52 +0100

    is this it?

--- trainer/src/optimiser.rs
+++ trainer/src/optimiser.rs
@@ -126,3 +126,109 @@ impl Optimiser for Adam {
         );
     }
 }
+
+pub struct AdamW {
+    velocity: Box<NNUEParams>,
+    momentum: Box<NNUEParams>,
+    decay: f32,
+}
+
+impl AdamW {
+    const B1: f32 = 0.9;
+    const B2: f32 = 0.999;
+
+    fn update(p: &mut f32, m: &mut f32, v: &mut f32, grad: f32, rate: f32, decay: f32) {
+        *m = Self::B1 * *m + (1. - Self::B1) * grad;
+        *v = Self::B2 * *v + (1. - Self::B2) * grad * grad;
+        *p *= decay;
+        *p -= rate * *m / (v.sqrt() + 0.000_000_01);
+    }
+}
+
+impl Default for AdamW {
+    fn default() -> Self {
+        Self {
+            velocity: NNUEParams::new(),
+            momentum: NNUEParams::new(),
+            decay: 0.01,
+        }
+    }
+}
+
+impl Optimiser for AdamW {
+    fn update_feature_weights(
+        &mut self,
+        params: &mut [f32; INPUT * HIDDEN],
+        grads: &[f32; INPUT * HIDDEN],
+        adj: f32,
+        rate: f32,
+    ) {
+        let decay = 1.0 - self.decay * rate;
+        for (i, param) in params.iter_mut().enumerate() {
+            let grad = adj * grads[i];
+            Self::update(
+                param,
+                &mut self.momentum.feature_weights[i],
+                &mut self.velocity.feature_weights[i],
+                grad,
+                rate,
+                decay,
+            );
+        }
+    }
+
+    fn update_feature_bias(
+        &mut self,
+        params: &mut [f32; HIDDEN],
+        grads: &[f32; HIDDEN],
+        adj: f32,
+        rate: f32,
+    ) {
+        let decay = 1.0 - self.decay * rate;
+        for (i, param) in params.iter_mut().enumerate() {
+            let grad = adj * grads[i];
+            Self::update(
+                param,
+                &mut self.momentum.feature_bias[i],
+                &mut self.velocity.feature_bias[i],
+                grad,
+                rate,
+                decay,
+            );
+        }
+    }
+
+    fn update_output_weights(
+        &mut self,
+        params: &mut [f32; 2 * HIDDEN],
+        grads: &[f32; 2 * HIDDEN],
+        adj: f32,
+        rate: f32,
+    ) {
+        let decay = 1.0 - self.decay * rate;
+        for (i, param) in params.iter_mut().enumerate() {
+            let grad = adj * grads[i];
+            Self::update(
+                param,
+                &mut self.momentum.output_weights[i],
+                &mut self.velocity.output_weights[i],
+                grad,
+                rate,
+                decay,
+            );
+        }
+    }
+
+    fn update_output_bias(&mut self, param: &mut f32, mut grad: f32, adj: f32, rate: f32) {
+        grad *= adj;
+        let decay = 1.0 - self.decay * rate;
+        Self::update(
+            param,
+            &mut self.momentum.output_bias,
+            &mut self.velocity.output_bias,
+            grad,
+            rate,
+            decay,
+        );
+    }
+}
