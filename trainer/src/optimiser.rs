f9bafcd09b59f6532b07eaf0c34b051b62a64c76
JacquesRW <jamie.whiting2003@icloud.com>
2023-09-02 22:09:29 +0100

    hm

--- trainer/src/optimiser.rs
+++ trainer/src/optimiser.rs
@@ -12,6 +12,13 @@ pub struct Adam {
 impl Adam {
     const B1: f32 = 0.9;
     const B2: f32 = 0.999;
+
+    fn update_single(&mut self, i: usize, param: &mut f32, grads: &NNUEParams, adj: f32, rate: f32) {
+        let grad = adj * grads.weights[i];
+        self.momentum[i] = Self::B1 * self.momentum[i] + (1. - Self::B1) * grad;
+        self.velocity[i] = Self::B2 * self.velocity[i] + (1. - Self::B2) * grad * grad;
+        *param -= rate * self.momentum[i] / (self.velocity[i].sqrt() + 0.000_000_01);
+    }
 }
 
 impl Default for Adam {
@@ -26,44 +33,23 @@ impl Default for Adam {
 impl Optimiser for Adam {
     fn update_weights(&mut self, nnue: &mut NNUEParams, grads: &NNUEParams, adj: f32, rate: f32) {
         for (i, param) in nnue.weights.iter_mut().enumerate() {
-            let grad = adj * grads.weights[i];
-            self.momentum[i] = Self::B1 * self.momentum[i] + (1. - Self::B1) * grad;
-            self.velocity[i] = Self::B2 * self.velocity[i] + (1. - Self::B2) * grad * grad;
-            *param -= rate * self.momentum[i] / (self.velocity[i].sqrt() + 0.000_000_01);
+            self.update_single(i, param, grads, adj, rate);
         }
     }
 }
 
+#[derive(Default)]
 pub struct AdamW {
-    velocity: Box<NNUEParams>,
-    momentum: Box<NNUEParams>,
+    adam: Adam,
     decay: f32,
-}
-
-impl AdamW {
-    const B1: f32 = 0.9;
-    const B2: f32 = 0.999;
-}
-
-impl Default for AdamW {
-    fn default() -> Self {
-        Self {
-            velocity: NNUEParams::new(),
-            momentum: NNUEParams::new(),
-            decay: 0.01,
-        }
-    }
 }
 
 impl Optimiser for AdamW {
     fn update_weights(&mut self, nnue: &mut NNUEParams, grads: &NNUEParams, adj: f32, rate: f32) {
         let decay = 1.0 - self.decay * rate;
         for (i, param) in nnue.weights.iter_mut().enumerate() {
-            let grad = adj * grads.weights[i];
-            self.momentum[i] = Self::B1 * self.momentum[i] + (1. - Self::B1) * grad;
-            self.velocity[i] = Self::B2 * self.velocity[i] + (1. - Self::B2) * grad * grad;
             *param *= decay;
-            *param -= rate * self.momentum[i] / (self.velocity[i].sqrt() + 0.000_000_01);
+            self.adam.update_single(i, param, grads, adj, rate);
         }
     }
 }
