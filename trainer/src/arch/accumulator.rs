f955e265d083dcf7b1af25571bb4c9cc0e94def6
JacquesRW <jamie.whiting2003@icloud.com>
2023-09-02 22:24:54 +0100

    imports

--- trainer/src/arch/accumulator.rs
+++ trainer/src/arch/accumulator.rs
@@ -1,26 +1,25 @@
 use super::nnue::NNUE;
-use std::ops::{AddAssign, Deref, Index, IndexMut};
 
 #[derive(Clone, Copy)]
 pub struct Accumulator<T, const SIZE: usize> {
     vals: [T; SIZE],
 }
 
-impl<T, const SIZE: usize> Deref for Accumulator<T, SIZE> {
+impl<T, const SIZE: usize> std::ops::Deref for Accumulator<T, SIZE> {
     type Target = [T; SIZE];
     fn deref(&self) -> &Self::Target {
         &self.vals
     }
 }
 
-impl<T, const SIZE: usize> Index<usize> for Accumulator<T, SIZE> {
+impl<T, const SIZE: usize> std::ops::Index<usize> for Accumulator<T, SIZE> {
     type Output = T;
     fn index(&self, index: usize) -> &Self::Output {
         &self.vals[index]
     }
 }
 
-impl<T, const SIZE: usize> IndexMut<usize> for Accumulator<T, SIZE> {
+impl<T, const SIZE: usize> std::ops::IndexMut<usize> for Accumulator<T, SIZE> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.vals[index]
     }
@@ -32,7 +31,7 @@ impl<T, const SIZE: usize> Accumulator<T, SIZE> {
     }
 }
 
-impl<T: Copy + AddAssign<T>, const SIZE: usize> Accumulator<T, SIZE> {
+impl<T: Copy + std::ops::AddAssign<T>, const SIZE: usize> Accumulator<T, SIZE> {
     pub fn add_feature(&mut self, feature_idx: usize, nnue: &NNUE<T>) {
         let start = feature_idx * SIZE;
         for (i, d) in self.vals.iter_mut().zip(&nnue[start..start + SIZE]) {
