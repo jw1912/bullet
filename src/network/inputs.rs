391a7d29cc6b36eac7cf70e9d00ed85574ac9c0f
JacquesRW <jamie.whiting2003@icloud.com>
2023-09-07 10:21:14 +0100

    maybe done?

--- src/network/inputs.rs
+++ src/network/inputs.rs
@@ -19,3 +19,20 @@ impl InputType for Chess768 {
         (wfeat, bfeat)
     }
 }
+
+pub struct HalfKA;
+impl InputType for HalfKA {
+    type FeatureType = (u8, u8, u8, u8);
+    const SIZE: usize = 64 * 768;
+
+    fn get_feature_indices((colour, piece, square, ksq): Self::FeatureType) -> (usize, usize) {
+        let bucket = 768 * usize::from(ksq);
+        let c = usize::from(colour);
+        let pc = 64 * usize::from(piece);
+        let sq = usize::from(square);
+        let wfeat = bucket + [0, 384][c] + pc + sq;
+        let bfeat = bucket + [384, 0][c] + pc + (sq ^ 56);
+        (wfeat, bfeat)
+    }
+}
+
