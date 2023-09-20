db3c2bf57ddb421c5cd5dcf38718c76538a0885a
Jamie Whiting <99771266+jw1912@users.noreply.github.com>
2023-09-20 01:48:40 +0100

    AVX512 SIMD (#57)

--- cpu/src/lib.rs
+++ cpu/src/lib.rs
@@ -1,7 +1,13 @@
+#![cfg_attr(feature = "simd", feature(stdsimd))]
+
+#[cfg(feature = "simd")]
+const _: () = assert!(HIDDEN % 32 == 0, "Must be a multiple of 64 for hand-written SIMD.");
+
 mod accumulator;
 mod gradient;
 mod optimiser;
 mod quantise;
+mod simd;
 
 pub use accumulator::Accumulator;
 pub use gradient::update_single_grad_cpu;
