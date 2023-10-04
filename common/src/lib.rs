58729bac3a7e4992fee5d63b7b305939b3224a21
Jamie Whiting <99771266+jw1912@users.noreply.github.com>
2023-10-04 17:09:58 +0100

    Custom Buckets Option (#65)

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -12,6 +12,7 @@ pub type Data = data::ChessBoard;
 /// Input format
 ///  - Chess768  (recommended)
 ///  - HalfKA
+///  - CustomBuckets
 pub type Input = inputs::Chess768;
 
 /// Size of hidden layer.
@@ -28,3 +29,20 @@ pub type Activation = activation::CReLU;
 ///  - Single    (recommended)
 ///  - MaterialCount<Buckets>
 pub type OutputBucket = outputs::Single;
+
+/// Applicable only with `Input` option
+/// "CustomBuckets", it is indexed from white POV,
+/// so index 0 corresponds to A1, 7 corresponds to H1,
+/// 56 corresponds to A8 and 63 corresponds to H8.
+/// With `N` buckets, the values in this array need to be
+/// in the set {0, 1, ..., N - 1}.
+pub const CUSTOM_BUCKETS: [usize; 64] = [
+    0, 0, 0, 0, 1, 1, 1, 1,
+    0, 0, 0, 0, 1, 1, 1, 1,
+    2, 2, 2, 2, 3, 3, 3, 3,
+    2, 2, 2, 2, 3, 3, 3, 3,
+    2, 2, 2, 2, 3, 3, 3, 3,
+    2, 2, 2, 2, 3, 3, 3, 3,
+    2, 2, 2, 2, 3, 3, 3, 3,
+    2, 2, 2, 2, 3, 3, 3, 3,
+];
