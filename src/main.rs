e677d65c5748e3b6164370b56c122cba3b58fae1
JacquesRW <jamie.whiting2003@icloud.com>
2023-12-31 17:32:00 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,8 @@ fn main() {
     let mut trainer = TrainerBuilder::default()
         .set_batch_size(16_384)
         .set_eval_scale(400.0)
-        .set_input(
-            inputs::ChessBuckets::new([
-                0, 0, 0, 0, 1, 1, 1, 1,
-                0, 0, 0, 0, 1, 1, 1, 1,
-                2, 2, 2, 2, 3, 3, 3, 3,
-                2, 2, 2, 2, 3, 3, 3, 3,
-                2, 2, 2, 2, 3, 3, 3, 3,
-                2, 2, 2, 2, 3, 3, 3, 3,
-                2, 2, 2, 2, 3, 3, 3, 3,
-                2, 2, 2, 2, 3, 3, 3, 3,
-            ])
-        )
-        .ft(768)
+        .set_input(inputs::Chess768)
+        .ft(256)
         .activate(Activation::SCReLU)
         .add_layer(1)
         .build();
