a65289b9feea843d14d23a4a8021596590d26014
JacquesRW <jamie.whiting2003@icloud.com>
2024-01-01 16:29:27 +0000

    ye

--- bullet-tensor/src/lib.rs
+++ bullet-tensor/src/lib.rs
@@ -2,6 +2,7 @@ mod buffer;
 mod optimiser;
 mod shape;
 mod sparse;
+mod tensor_batch;
 mod tensor;
 
 #[cfg(test)]
@@ -12,7 +13,8 @@ pub use buffer::GpuBuffer;
 pub use optimiser::Optimiser;
 pub use shape::Shape;
 pub use sparse::SparseTensor;
-pub use tensor::{Tensor, TensorBatch};
+pub use tensor_batch::TensorBatch;
+pub use tensor::Tensor;
 pub use bullet_cuda::{CublasHandle, util::{device_synchronise, panic_if_cuda_error}};
 
 #[derive(Clone, Copy, Debug)]
---
+++ bullet-tensor/src/tensor_batch.rs
@@ -0,0 +1,245 @@
+use std::ffi::c_int;
+
+use bullet_cuda::{ops, CublasHandle};
+
+use crate::{Activation, GpuBuffer, Shape, Tensor};
+
+pub struct TensorBatch {
+    shape: Shape,
+    cap: usize,
+    buf: GpuBuffer,
+}
+
+impl TensorBatch {
+    /// Creates a new tensor with given `shape` and `cap`
+    /// buffer length, with a zeroed buffer on the GPU
+    pub fn new(shape: Shape, cap: usize) -> Self {
+        assert!(cap > 0, "Cannot have a 0 sized batch!");
+
+        Self {
+            shape,
+            cap,
+            buf: GpuBuffer::new(cap * shape.size()),
+        }
+    }
+
+    pub fn shape(&self) -> Shape {
+        self.shape
+    }
+
+    pub fn cap(&self) -> usize {
+        self.cap
+    }
+
+    pub fn is_empty(&self) -> bool {
+        false
+    }
+
+    pub(crate) fn ptr(&self) -> *mut f32 {
+        self.buf.ptr()
+    }
+
+    pub fn element_size(&self) -> usize {
+        self.shape.size()
+    }
+
+    pub fn num_elements(&self) -> usize {
+        self.buf.size()
+    }
+
+    pub fn load_from_cpu(&self, buf: &[f32]) {
+        self.buf.load_from_cpu(buf);
+    }
+
+    pub fn offset_load_from_cpu(&self, buf: &[f32], offset: usize) {
+        self.buf.offset_load_from_cpu(buf, offset);
+    }
+
+    pub fn write_to_cpu(&self, buf: &mut [f32]) {
+        self.buf.write_to_cpu(buf);
+    }
+
+    /// # Safety
+    /// `a` must be initialised, all other sources of unsafety
+    /// should trip an assert.
+    pub unsafe fn splat_mul_matrix_vector(
+        handle: CublasHandle,
+        batch_size: usize,
+        a: &Tensor,
+        x: &TensorBatch,
+        y: &TensorBatch,
+    ) {
+        let (m, n) = validate_dims(a.shape(), x, y);
+
+        ops::mul_matrix_vector::<false>(
+            handle,
+            m,
+            n,
+            a.ptr(),
+            0,
+            x.ptr(),
+            y.ptr(),
+            batch_size as c_int,
+        );
+    }
+
+    /// # Safety
+    /// `a` must be initialised, all other sources of unsafety
+    /// should trip an assert.
+    pub unsafe fn splat_mul_matrixt_vector(
+        handle: CublasHandle,
+        batch_size: usize,
+        a: &Tensor,
+        y: &TensorBatch,
+        x: &TensorBatch,
+    ) {
+        let (m, n) = validate_dims(a.shape(), x, y);
+
+        ops::mul_matrix_vector::<true>(
+            handle,
+            m,
+            n,
+            a.ptr(),
+            0,
+            y.ptr(),
+            x.ptr(),
+            batch_size as c_int,
+        );
+    }
+
+    pub fn mul_vector_vectort(
+        handle: CublasHandle,
+        batch_size: usize,
+        y: &TensorBatch,
+        x: &TensorBatch,
+        a: &TensorBatch,
+    ) {
+        let a_shape = a.shape();
+        assert_eq!(x.shape(), Shape::new(1, a_shape.cols()));
+        assert_eq!(y.shape(), Shape::new(1, a_shape.rows()));
+        assert_eq!(x.cap(), y.cap(), "Not all tensor caps are the same length!");
+        assert_eq!(x.cap(), a.cap(), "Not all tensor caps are the same length!");
+        assert!(batch_size <= x.cap());
+
+        let m = a_shape.cols() as c_int;
+        let n = a_shape.rows() as c_int;
+
+        unsafe {
+            ops::mul_vector_vectort(
+                handle,
+                m,
+                n,
+                y.ptr(),
+                x.ptr(),
+                a.ptr(),
+                a.element_size() as c_int,
+                batch_size as c_int,
+            );
+        }
+    }
+
+    /// Modifies a batch of tensors.
+    fn map(
+        f: unsafe extern "C" fn(usize, *const f32, *mut f32),
+        batch_size: usize,
+        inp: &TensorBatch,
+        out: &TensorBatch,
+    ) {
+        assert_eq!(inp.shape(), out.shape(), "Mismatched tensor shapes!");
+        assert_eq!(inp.cap(), out.cap(), "Mismatched cap sizes!");
+        assert!(batch_size <= inp.cap(), "Overflow!");
+        unsafe {
+            f(batch_size * inp.element_size(), inp.ptr(), out.ptr());
+        }
+    }
+
+    /// This calulates `out[i] = op(inp[i])` for a batch of input.
+    pub fn activate(batch_size: usize, op: Activation, inp: &TensorBatch, out: &TensorBatch) {
+        match op {
+            Activation::ReLU => Self::map(ops::activate_relu, batch_size, inp, out),
+            Activation::CReLU => Self::map(ops::activate_crelu, batch_size, inp, out),
+            Activation::SCReLU => Self::map(ops::activate_screlu, batch_size, inp, out),
+        }
+    }
+
+    /// This calulates `out[i] = inp[i] * op'(out[i])` for a batch of input.
+    pub fn backprop_activation(batch_size: usize, op: Activation, inp: &TensorBatch, out: &TensorBatch) {
+        match op {
+            Activation::ReLU => Self::map(ops::backprop_relu, batch_size, inp, out),
+            Activation::CReLU => Self::map(ops::backprop_crelu, batch_size, inp, out),
+            Activation::SCReLU => Self::map(ops::backprop_screlu, batch_size, inp, out),
+        }
+    }
+
+    /// # Safety
+    /// `weights` and `biases` must be initialised.
+    pub unsafe fn affine(
+        handle: CublasHandle,
+        batch_size: usize,
+        weights: &Tensor,
+        inputs: &TensorBatch,
+        biases: &Tensor,
+        outputs: &TensorBatch,
+    ) {
+        TensorBatch::splat_mul_matrix_vector(handle, batch_size, weights, inputs, outputs);
+        TensorBatch::splat_add(batch_size, biases, outputs);
+    }
+
+    /// # Safety
+    /// `weights` must be initialised.
+    #[allow(clippy::too_many_arguments)]
+    pub unsafe fn backprop_affine(
+        handle: CublasHandle,
+        ones: &GpuBuffer,
+        batch_size: usize,
+        weights: &Tensor,
+        errors: &TensorBatch,
+        inputs: &TensorBatch,
+        weights_grad: &Tensor,
+        biases_grad: &Tensor,
+        weights_intermediate: &TensorBatch,
+    ) {
+        TensorBatch::mul_vector_vectort(handle, batch_size, errors, inputs, weights_intermediate);
+        TensorBatch::reduce_add(handle, ones, batch_size, weights_intermediate, weights_grad);
+        TensorBatch::reduce_add(handle, ones, batch_size, errors, biases_grad);
+        TensorBatch::splat_mul_matrixt_vector(handle, batch_size, weights, errors, inputs);
+    }
+
+    /// # Safety
+    /// `out` must be pointing to valid allocated memory.
+    pub unsafe fn reduce_add(
+        handle: CublasHandle,
+        ones: &GpuBuffer,
+        batch_size: usize,
+        inp: &TensorBatch,
+        out: &Tensor,
+    ) {
+        assert_eq!(inp.shape(), out.shape());
+        ops::reduce_add(handle, ones.ptr(), batch_size, out.num_elements(), inp.ptr(), out.ptr());
+    }
+
+    /// # Safety
+    /// `inp` must be pointing to valid allocated memory.
+    pub unsafe fn splat_add(batch_size: usize, inp: &Tensor, out: &TensorBatch) {
+        assert_eq!(inp.shape(), out.shape());
+        ops::splat_add(batch_size, out.element_size(), inp.ptr(), out.ptr());
+    }
+
+    pub fn sigmoid_mse(&self, batch_size: usize, results: &TensorBatch, error: &GpuBuffer) {
+        assert_eq!(error.size(), 1);
+        assert_eq!(self.shape(), results.shape());
+        assert_eq!(self.element_size(), results.element_size());
+
+        unsafe {
+            ops::sigmoid_mse(batch_size, self.ptr(), results.ptr(), error.ptr());
+        }
+    }
+}
+
+fn validate_dims(a_shape: Shape, x: &TensorBatch, y: &TensorBatch) -> (c_int, c_int) {
+    assert_eq!(x.shape(), Shape::new(1, a_shape.cols()));
+    assert_eq!(y.shape(), Shape::new(1, a_shape.rows()));
+    assert_eq!(x.cap(), y.cap(), "Not all tensor caps are the same length!");
+
+    (a_shape.cols() as c_int, a_shape.rows() as c_int)
+}
