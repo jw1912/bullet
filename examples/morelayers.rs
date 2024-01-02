03e1c78a9debb0c28c14ad43870aeba98e7d7804
JacquesRW <jamie.whiting2003@icloud.com>
2024-01-02 11:45:13 +0000

    >1 Layer Example

---
+++ examples/morelayers.rs
@@ -0,0 +1,35 @@
+use bullet::{
+    inputs, run_training, Activation, LrScheduler, LrSchedulerType, TrainerBuilder,
+    TrainingSchedule, WdlScheduler,
+};
+
+fn main() {
+    let mut trainer = TrainerBuilder::default()
+        .set_batch_size(16_384)
+        .set_eval_scale(400.0)
+        .set_quantisations(&[181, 128, 64])
+        .set_input(inputs::Chess768)
+        .ft(768)
+        .activate(Activation::SCReLU)
+        .add_layer(16)
+        .activate(Activation::ReLU)
+        .add_layer(1)
+        .build();
+
+    let mut schedule = TrainingSchedule {
+        net_id: "morelayers".to_string(),
+        start_epoch: 1,
+        num_epochs: 20,
+        wdl_scheduler: WdlScheduler::new(0.2, 0.5),
+        lr_scheduler: LrScheduler::new(0.001, 0.1, LrSchedulerType::Step(8)),
+        save_rate: 1,
+    };
+
+    run_training(
+        &mut trainer,
+        &mut schedule,
+        4,
+        "../../data/akimbo3-9.data",
+        "checkpoints",
+    );
+}
