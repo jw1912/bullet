c78ca837629a91cfed9710c4c19663c8d160202e
JacquesRW <jamie.whiting2003@icloud.com>
2024-01-08 23:36:18 +0000

    Handwritten done

--- bullet-cpu/src/ops/sparse_affine.rs
+++ bullet-cpu/src/ops/sparse_affine.rs
@@ -12,7 +12,45 @@ pub unsafe fn sparse_affine_forward(
     inputs: *const Feat,
     outputs: *mut f32,
 ) {
-    unimplemented!();
+    let weights = weights as usize;
+    let biases = biases as usize;
+    let inputs = inputs as usize;
+    let outputs = outputs as usize;
+
+    handle.split_workload(batch_size, |_, idx| {
+        let weights = weights as *const f32;
+        let biases = biases as *const f32;
+        let this_inp = (inputs as *const Feat).add(max_input_size * idx);
+        let our_out = (outputs as *mut f32).add(2 * output_size * idx);
+        let opp_out = our_out.add(output_size);
+
+        for i in 0..output_size {
+            *our_out.add(i) = *biases.add(i);
+        }
+
+        for i in 0..output_size {
+            *opp_out.add(i) = *biases.add(i);
+        }
+
+        for i in 0..max_input_size {
+            let feat = *this_inp.add(i);
+            println!("{idx}: {}, {}", feat.our(), feat.opp());
+
+            if feat.our() == 65_535 {
+                break;
+            }
+
+            let our_weights = weights.add(output_size * feat.our());
+            for j in 0..output_size {
+                *our_out.add(j) += *our_weights.add(j);
+            }
+
+            let opp_weights = weights.add(output_size * feat.opp());
+            for j in 0..output_size {
+                *opp_out.add(j) += *opp_weights.add(j);
+            }
+        }
+    });
 }
 
 pub unsafe fn sparse_affine_backward(
@@ -25,5 +63,36 @@ pub unsafe fn sparse_affine_backward(
     inputs: *const Feat,
     errors: *const f32,
 ) {
-    unimplemented!();
+    for idx in 0..batch_size {
+        let this_inp = inputs.add(max_input_size * idx);
+        let this_err = errors.add(2 * output_size * idx);
+        let our_err = this_err;
+        let opp_err = this_err.add(output_size);
+
+        for i in 0..output_size {
+            *biases_grad.add(i) += *our_err.add(i);
+        }
+
+        for i in 0..output_size {
+            *biases_grad.add(i) += *opp_err.add(i);
+        }
+
+        for i in 0..max_input_size {
+            let feat = *this_inp.add(i);
+
+            if feat.our() == 65_535 {
+                break;
+            }
+
+            let our_weights = weights_grad.add(output_size * feat.our());
+            for j in 0..output_size {
+                *our_weights.add(j) += *our_err.add(j);
+            }
+
+            let opp_weights = weights_grad.add(output_size * feat.opp());
+            for j in 0..output_size {
+                *opp_weights.add(j) += *opp_err.add(j);
+            }
+        }
+    }
 }
