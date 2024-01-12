5dc7243585fbcace06a8975a6b35e2639c4d255a
Jamie Whiting <99771266+jw1912@users.noreply.github.com>
2024-01-12 01:33:51 +0000

    Optimise CPU Backend More (#84)

--- examples/akimbo-main.rs
+++ examples/akimbo-main.rs
@@ -39,4 +39,9 @@ fn main() {
     };
 
     trainer.run(&schedule, &settings);
+    trainer.eval("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 | 0 | 0.0");
+    trainer.eval("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 | 0 | 0.0");
+    trainer.eval("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1 | 0 | 0.0");
+    trainer.eval("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8 | 0 | 0.0");
+    trainer.eval("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1 | 0 | 0.0");
 }
