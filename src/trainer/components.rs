ee7888635bd1e8f854df42fb09791594e28094dc
JacquesRW <jamie.whiting2003@icloud.com>
2024-03-20 21:17:36 +0000

    move trainer into folder

---
+++ src/trainer/components.rs
@@ -0,0 +1,35 @@
+use crate::{Activation, tensor::{DeviceBuffer, Tensor, TensorBatch}};
+
+pub(super) struct FeatureTransformer {
+    pub weights: Tensor,
+    pub biases: Tensor,
+    pub weights_grad: Tensor,
+    pub biases_grad: Tensor,
+    pub single_perspective: bool,
+    pub outputs: TensorBatch,
+    pub copy: TensorBatch,
+}
+
+pub(super) struct Affine {
+    pub weights: Tensor,
+    pub biases: Tensor,
+    pub weights_grad: Tensor,
+    pub biases_grad: Tensor,
+    pub ones: DeviceBuffer,
+}
+
+pub(super) enum Operation {
+    Activate(Activation),
+    Affine(Affine),
+    Select,
+}
+
+pub(super) struct Node {
+    pub outputs: TensorBatch,
+    pub op: Operation,
+}
+
+pub(super) struct QuantiseInfo {
+    pub val: i32,
+    pub start: usize,
+}
