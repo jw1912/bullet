79ac61dd33a0e073000f1fa9a57d8f3dd8269171
Rak Laptudirm <rak@laptudirm.com>
2024-08-03 16:55:31 +0530

    chore: rollback some unnecessary changes 3

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,19 +1,24 @@
-#[cfg(not(any(feature = "cuda", feature = "metal", feature = "hip")))]
-pub use cpu::*;
-#[cfg(feature = "cuda")]
-pub use cuda::*;
-#[cfg(feature = "hip")]
-pub use hip::*;
-#[cfg(feature = "metal")]
-pub use metal::*;
-
-pub mod cpu;
-
 #[cfg(feature = "cuda")]
 mod cuda;
+
+#[cfg(feature = "cuda")]
+pub use cuda::*;
 
 #[cfg(feature = "hip")]
 mod hip;
 
+#[cfg(feature = "hip")]
+pub use hip::*;
+
 #[cfg(feature = "metal")]
 mod metal;
+
+#[cfg(feature = "metal")]
+pub use metal::*;
+
+// feature = "metal" depends on the cpu module.
+#[cfg(not(any(feature = "cuda", feature = "hip")))]
+pub mod cpu;
+
+#[cfg(not(any(feature = "cuda", feature = "metal", feature = "hip")))]
+pub use cpu::*;
