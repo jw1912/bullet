c64e2e34d68aec7c6a055b2d342671cf7c43b0ff
Rak Laptudirm <rak@laptudirm.com>
2024-08-06 10:24:35 +0530

    chore: fix code formatting

--- src/backend/metal/util.rs
+++ src/backend/metal/util.rs
@@ -4,8 +4,11 @@ The things you have to do for a heterogenous interface...
 
 use metal_rs::Device;
 
-pub use crate::backend::cpu::util::{calloc, copy_from_device, copy_on_device, copy_to_device, device_synchronise, free, malloc, panic_if_device_error, set_zero};
+pub use crate::backend::cpu::util::{
+    calloc, copy_from_device, copy_on_device, copy_to_device, device_synchronise, free, malloc, panic_if_device_error,
+    set_zero,
+};
 
 pub fn device_name() -> String {
-	Device::system_default().unwrap().name().to_string()
+    Device::system_default().unwrap().name().to_string()
 }
