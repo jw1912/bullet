4b749df1c79a444ec946bd3eaae56f33e9c7a00a
jw1912 <jamie.whiting.dev@gmail.com>
2024-10-04 22:51:04 +0100

    fmt

--- src/operations.rs
+++ src/operations.rs
@@ -34,11 +34,11 @@ pub fn select(builder: &mut GraphBuilder, input1: Node, input2: Node) -> Node {
 }
 
 /// This fuses the following operations
-/// 
+///
 /// ` stm_accumulator = activate(affine(weights,  stm, bias))`
-/// 
+///
 /// `nstm_accumulator = activate(affine(weights, nstm, bias))`
-/// 
+///
 /// `out = concat(stm_accumulator, nstm_accumulator)`
 pub fn sparse_affine_dual_with_activation(
     builder: &mut GraphBuilder,
@@ -53,7 +53,7 @@ pub fn sparse_affine_dual_with_activation(
 
 /// Post sparse-affine-dual, doing pairwise would just elementise-mul the stm and nstm
 /// accumulators, which is not what is wanted.
-/// 
+///
 /// This will perform the pairwise mul within each accumulator.
 pub fn pairwise_mul_post_sparse_affine_dual(builder: &mut GraphBuilder, input: Node) -> Node {
     builder.create_result_of_operation(Operation::PairwiseMul(true), &[input])
