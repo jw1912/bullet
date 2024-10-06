ad1fb55cee62bfca8d5633ce9d299b876fcddd32
Jamie Whiting <99771266+jw1912@users.noreply.github.com>
2024-10-06 20:42:57 +0100

    Fix typo + Clarify an example (#152)

--- examples/simple.rs
+++ examples/simple.rs
@@ -29,14 +29,14 @@ fn main() {
 
     let schedule = TrainingSchedule {
         net_id: "simple".to_string(),
-        eval_scale: 400.0,
+        eval_scale: SCALE as f32,
         steps: TrainingSteps {
             batch_size: 16_384,
             batches_per_superbatch: 6104,
             start_superbatch: 1,
             end_superbatch: 20,
         },
-        wdl_scheduler: wdl::ConstantWDL { value: 0.0 },
+        wdl_scheduler: wdl::ConstantWDL { value: 0.75 },
         lr_scheduler: lr::StepLR { start: 0.001, gamma: 0.1, step: 8 },
         save_rate: 10,
     };
