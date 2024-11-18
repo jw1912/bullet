c41f202ad4ab46e1d557ae2b850ea79d7a398e0d
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-11-18 22:44:40 +0100

    Updates

---
+++ convert.bat
@@ -0,0 +1,2 @@
+bullet-utils.exe convert --from text --input output.txt --output data.bin --threads 8
+del output.txt
--- examples/simple.rs
+++ examples/simple.rs
@@ -10,7 +10,7 @@ use bullet_lib::{
     TrainingSteps,
 };
 
-const HIDDEN_SIZE: usize = 256;
+const HIDDEN_SIZE: usize = 512;
 const SCALE: i32 = 400;
 const QA: i16 = 255;
 const QB: i16 = 64;
@@ -27,7 +27,7 @@ fn main() {
         .add_layer(1)
         .build();
 
-    //trainer.load_from_checkpoint("C:\\NNUE-Trainer\\checkpoints\\simple-1090\\");
+    //trainer.load_from_checkpoint("C:\\NNUE-Trainer\\checkpoints\\simple-165\\");
 
     let schedule = TrainingSchedule {
         net_id: "simple".to_string(),
@@ -38,7 +38,7 @@ fn main() {
             start_superbatch: 1,
             end_superbatch: 500,
         },
-        wdl_scheduler: wdl::ConstantWDL { value: 100.0 },
+        wdl_scheduler: wdl::ConstantWDL { value: 0.0 },
         lr_scheduler: lr::StepLR { start: 0.001, gamma: 0.3, step: 125 },
         save_rate: 5,
     };
@@ -50,7 +50,7 @@ fn main() {
 
     let settings = LocalSettings { threads: 4, test_set: None, output_directory: "checkpoints", batch_queue_size: 512 };
 
-    let data_loader = loader::DirectSequentialDataLoader::new(&["C:\\NNUE-Trainer\\data.bin"]);
+    let data_loader = loader::DirectSequentialDataLoader::new(&["C:\\NNUE-Trainer\\data-1.bin", "C:\\NNUE-Trainer\\data.bin"]);
 
     trainer.run(&schedule, &settings, &data_loader);
 }
