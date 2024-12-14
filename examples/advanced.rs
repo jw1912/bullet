fc74be4c84a8fd2a84399d5c1e97ae29138011d0
jw1912 <jamie.whiting.dev@gmail.com>
2024-12-14 20:06:02 +0000

    .

--- examples/advanced.rs
+++ examples/advanced.rs
@@ -41,7 +41,7 @@ fn main() {
             batch_size: 16_384,
             batches_per_superbatch: 6104,
             start_superbatch: 1,
-            end_superbatch: 1,
+            end_superbatch: 240,
         },
         wdl_scheduler: wdl::ConstantWDL { value: 0.0 },
         lr_scheduler: lr::StepLR { start: 0.001, gamma: 0.3, step: 60 },
