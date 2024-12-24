16c09c6464ce811a01f8e649906662d1c6927410
Ciekce <kantaja98@gmail.com>
2024-12-24 20:31:15 +0000

    default to halfkav2(_hm)

--- src/trainer/default/inputs/chess_buckets_mk.rs
+++ src/trainer/default/inputs/chess_buckets_mk.rs
@@ -8,6 +8,21 @@ pub struct ChessBucketsMergedKings {
     num_buckets: usize,
 }
 
+impl Default for ChessBucketsMergedKings {
+    fn default() -> Self {
+        Self::new([
+             0,  1,  2,  3,
+             4,  5,  6,  7,
+             8,  9, 10, 11,
+            12, 13, 14, 15,
+            16, 17, 18, 19,
+            20, 21, 22, 23,
+            24, 25, 26, 27,
+            28, 29, 30, 31,
+        ])
+    }
+}
+
 impl ChessBucketsMergedKings {
     pub fn new(buckets: [usize; 64]) -> Self {
         let num_buckets = get_num_buckets(&buckets);
