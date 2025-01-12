ba654911cef0076ee98a297145f0af35d2918f08
jw1912 <jamie.whiting.dev@gmail.com>
2025-01-12 23:47:08 +0000

    Simplify build script

--- kernels/gather.cu
+++ kernels/gather.cu
@@ -3,7 +3,6 @@
 __global__ void gather_kernel(
     const size_t input_rows,
     const size_t output_rows,
-    const size_t cols,
     const float* inputs,
     const int32_t* indices,
     float* outputs)
@@ -21,7 +20,6 @@ __global__ void gather_kernel(
 __global__ void gather_backprop_kernel(
     const size_t input_rows,
     const size_t output_rows,
-    const size_t cols,
     const float* output_grads,
     const int32_t* indices,
     float* input_grads)
@@ -50,7 +48,7 @@ extern "C" void gather(
     const size_t chunks = (output_rows + threads - 1) / threads;
     dim3 grid(chunks, cols);
 
-    gather_kernel<<<grid, threads>>>(input_rows, output_rows, cols, inputs, indices, outputs);
+    gather_kernel<<<grid, threads>>>(input_rows, output_rows, inputs, indices, outputs);
 }
 
 extern "C" void gather_backprop(
@@ -65,5 +63,5 @@ extern "C" void gather_backprop(
     const size_t chunks = (output_rows + threads - 1) / threads;
     dim3 grid(chunks, cols);
 
-    gather_backprop_kernel<<<grid, threads>>>(input_rows, output_rows, cols, output_grads, indices, input_grads);
+    gather_backprop_kernel<<<grid, threads>>>(input_rows, output_rows, output_grads, indices, input_grads);
 }
