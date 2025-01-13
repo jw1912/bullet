fed31d10e91db26264ac9321323469e2f9f19378
jw1912 <jamie.whiting.dev@gmail.com>
2025-01-13 21:02:56 +0000

    .

--- kernels/select.cu
+++ kernels/select.cu
@@ -4,25 +4,27 @@
 #endif
 
 __global__ void selectKernel(
-    const size_t batchSize,
-    const size_t inputSize,
-    const size_t outputSize,
+    const size_t batch_size,
+    const size_t input_size,
+    const size_t output_size,
     const int32_t* buckets,
     const float* in,
     float* out)
 {
-    const size_t thisIdx = blockIdx.x * blockDim.x + threadIdx.x;
+    const size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (thisIdx >= batchSize)
+    if (tid >= batch_size * output_size)
         return;
 
-    const size_t thisBucket = static_cast<size_t>(buckets[thisIdx]);
+    const size_t idxInBatch = tid / output_size;
+    const size_t idxInOutput = tid % output_size;
 
-    const float* thisInput = in + inputSize * thisIdx + outputSize * thisBucket;
-    float* thisOutput = out + outputSize * thisIdx;
+    const size_t thisBucket = static_cast<size_t>(buckets[idxInBatch]);
 
-    for (size_t i = 0; i < outputSize; i++)
-        thisOutput[i] = thisInput[i];
+    const float* thisInput = in + input_size * idxInBatch + output_size * thisBucket + idxInOutput;
+    float* thisOutput = out + output_size * idxInBatch + idxInOutput;
+
+    thisOutput[0] = thisInput[0];
 }
 
 __global__ void selectBackpropKernel(
@@ -33,35 +35,36 @@ __global__ void selectBackpropKernel(
     const float* output_grad,
     float* input_grad)
 {
-    const size_t thisIdx = blockIdx.x * blockDim.x + threadIdx.x;
+    const size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (thisIdx >= batch_size)
+    if (tid >= batch_size * output_size)
         return;
 
-    const size_t thisBucket = static_cast<size_t>(buckets[thisIdx]);
+    const size_t idxInBatch = tid / output_size;
+    const size_t idxInOutput = tid % output_size;
 
-    const float* thisOutputGrad = output_grad + output_size * thisIdx;
-    float* thisInputGrad = input_grad + input_size * thisIdx + output_size * thisBucket;
+    const size_t thisBucket = static_cast<size_t>(buckets[idxInBatch]);
 
-    for (size_t i = 0; i < output_size; i++)
-        thisInputGrad[i] += thisOutputGrad[i];
+    const float* thisOutputGrad = output_grad + output_size * idxInBatch + idxInOutput;
+    float* thisInputGrad = input_grad + input_size * idxInBatch + output_size * thisBucket + idxInOutput;
+
+    thisInputGrad[0] += thisOutputGrad[0];
 }
 
 extern "C" void selectForward(
-    const size_t batchSize,
-    const size_t inputSize,
-    const size_t outputSize,
+    const size_t batch_size,
+    const size_t input_size,
+    const size_t output_size,
     const int32_t* buckets,
     const float* in,
     float* out)
 {
-    const size_t numChunks = (batchSize + threadsPerBlock - 1) / threadsPerBlock;
-    const size_t threads = (numChunks == 1) ? batchSize : threadsPerBlock;
+    const size_t blocks = (batch_size * output_size + threadsPerBlock - 1) / threadsPerBlock;
 
-    selectKernel<<<numChunks, threads>>>(
-        batchSize,
-        inputSize,
-        outputSize,
+    selectKernel<<<blocks, threadsPerBlock>>>(
+        batch_size,
+        input_size,
+        output_size,
         buckets,
         in,
         out
@@ -76,10 +79,9 @@ extern "C" void selectBackprop(
     const float* output_grad,
     float* input_grad)
 {
-    const size_t numChunks = (batch_size + threadsPerBlock - 1) / threadsPerBlock;
-    const size_t threads = (numChunks == 1) ? batch_size : threadsPerBlock;
+    const size_t blocks = (batch_size * output_size + threadsPerBlock - 1) / threadsPerBlock;
 
-    selectBackpropKernel<<<numChunks, threads>>>(
+    selectBackpropKernel<<<blocks, threadsPerBlock>>>(
         batch_size,
         input_size,
         output_size,
