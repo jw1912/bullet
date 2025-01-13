00e9f152f531ad9ce81212675924e8af2e84c01f
Jamie Whiting <99771266+jw1912@users.noreply.github.com>
2025-01-13 20:51:31 +0000

    Optimise Pairwise Mul Kernels (#253)

--- kernels/pairwise.cu
+++ kernels/pairwise.cu
@@ -3,29 +3,44 @@
 #include <hip/hip_runtime.h>
 #endif
 
-__global__ void pairwiseMulKernel(const size_t output_size, const float* input, float* output)
+__global__ void pairwiseMulKernel(
+    const size_t output_size,
+    const size_t batch_size,
+    const float* input,
+    float* output)
 {
     const size_t tid = blockDim.x * blockIdx.x + threadIdx.x;
 
-    if (tid >= output_size)
+    if (tid >= output_size * batch_size)
         return;
 
-    const float* thisInp = input + 2 * output_size * blockIdx.y + tid;
-    float* thisOut = output + output_size * blockIdx.y + tid;
+    const size_t idxInBatch = tid / output_size;
+    const size_t idxInOutput = tid % output_size;
+
+    const float* thisInp = input + 2 * output_size * idxInBatch + idxInOutput;
+    float* thisOut = output + output_size * idxInBatch + idxInOutput;
 
     thisOut[0] = thisInp[0] * thisInp[output_size];
 }
 
-__global__ void pairwiseMulBackwardKernel(const size_t output_size, const float* input, const float* output_grad, float* input_grad)
+__global__ void pairwiseMulBackwardKernel(
+    const size_t output_size,
+    const size_t batch_size,
+    const float* input,
+    const float* output_grad,
+    float* input_grad)
 {
     const size_t tid = blockDim.x * blockIdx.x + threadIdx.x;
 
-    if (tid >= output_size)
+    if (tid >= output_size * batch_size)
         return;
 
-    const float* thisOutputGrad = output_grad + output_size * blockIdx.y + tid;
+    const size_t idxInBatch = tid / output_size;
+    const size_t idxInOutput = tid % output_size;
+
+    const float* thisOutputGrad = output_grad + output_size * idxInBatch + idxInOutput;
     
-    const size_t inputOffset = 2 * output_size * blockIdx.y + tid;
+    const size_t inputOffset = 2 * output_size * idxInBatch + idxInOutput;
     const float* thisInput = input + inputOffset;
     float* thisInputGrad = input_grad + inputOffset;
 
@@ -37,16 +52,14 @@ __global__ void pairwiseMulBackwardKernel(const size_t output_size, const float*
 
 extern "C" void pairwiseMul(const size_t batch_size, const size_t output_size, const float* input, float* output)
 {
-    const size_t grid_x = (output_size + threadsPerBlock - 1) / threadsPerBlock;
-    const dim3 grid(grid_x, batch_size);
-
-    pairwiseMulKernel<<<grid, threadsPerBlock>>>(output_size, input, output);
+    const size_t total_outputs = batch_size * output_size;
+    const size_t blocks = (total_outputs + threadsPerBlock - 1) / threadsPerBlock;
+    pairwiseMulKernel<<<blocks, threadsPerBlock>>>(output_size, batch_size, input, output);
 }
 
 extern "C" void backpropPairwiseMul(const size_t batch_size, const size_t output_size, const float* input, const float* output_grad, float* input_grad)
 {
-    const size_t grid_x = (output_size + threadsPerBlock - 1) / threadsPerBlock;
-    const dim3 grid(grid_x, batch_size);
-
-    pairwiseMulBackwardKernel<<<grid, threadsPerBlock>>>(output_size, input, output_grad, input_grad);
+    const size_t total_outputs = batch_size * output_size;
+    const size_t blocks = (total_outputs + threadsPerBlock - 1) / threadsPerBlock;
+    pairwiseMulBackwardKernel<<<blocks, threadsPerBlock>>>(output_size, batch_size, input, output_grad, input_grad);
 }
