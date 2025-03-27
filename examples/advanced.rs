36ae0c31a10a8e241341f15ec7cc3dbb8228fcdc
jw1912 <jamie.whiting.dev@gmail.com>
2025-03-27 20:56:52 +0000

    lora

--- examples/advanced.rs
+++ examples/advanced.rs
@@ -1,47 +1,32 @@
 use bullet_lib::{
     nn::{
         optimiser::{AdamWOptimiser, AdamWParams},
-        ExecutionContext, Graph, InitSettings, NetworkBuilder, Node, Shape,
+        ExecutionContext, Graph, GraphCompileArgs, InitSettings, NetworkBuilder, Node, Shape,
     },
     trainer::{
-        default::{
-            inputs::{self, SparseInputType},
-            loader, outputs, Trainer,
-        },
-        save::{Layout, QuantTarget, SavedFormat},
+        default::{inputs, loader, outputs, Trainer},
         schedule::{lr, wdl, TrainingSchedule, TrainingSteps},
         settings::LocalSettings,
     },
 };
 
 type InputFeatures = inputs::Chess768;
-const HL_SIZE: usize = 512;
-const OUTPUT_BUCKETS: usize = 8;
+const INPUTS: usize = 768;
 
 fn main() {
     let inputs = InputFeatures::default();
-    let num_inputs = inputs.num_inputs();
-    let max_active = inputs.max_active();
 
-    let (graph, output_node) = build_network(num_inputs, max_active, OUTPUT_BUCKETS, HL_SIZE);
+    let (graph, output_node) = build_lora(128, 4);
+
+    println!("Number of weights: {}", graph.get_num_params());
 
     let mut trainer = Trainer::<AdamWOptimiser, _, _>::new(
         graph,
         output_node,
         AdamWParams::default(),
         inputs,
-        outputs::MaterialCount::<OUTPUT_BUCKETS>,
-        vec![
-            SavedFormat::new("pst", QuantTarget::I16(255), Layout::Normal),
-            SavedFormat::new("l0w", QuantTarget::I16(255), Layout::Normal),
-            SavedFormat::new("l0b", QuantTarget::I16(255), Layout::Normal),
-            SavedFormat::new("l1w", QuantTarget::I16(64), Layout::Normal),
-            SavedFormat::new("l1b", QuantTarget::I16(64 * 255), Layout::Normal),
-            SavedFormat::new("l2w", QuantTarget::Float, Layout::Normal),
-            SavedFormat::new("l2b", QuantTarget::Float, Layout::Normal),
-            SavedFormat::new("l3w", QuantTarget::Float, Layout::Normal),
-            SavedFormat::new("l3b", QuantTarget::Float, Layout::Normal),
-        ],
+        outputs::Single,
+        vec![],
         false,
     );
 
@@ -69,46 +54,48 @@ fn main() {
     println!("Eval: {eval:.3}cp");
 }
 
-fn build_network(num_inputs: usize, max_active: usize, num_buckets: usize, hl: usize) -> (Graph, Node) {
-    let builder = NetworkBuilder::default();
+#[allow(unused)]
+fn build_simple(hl: usize) -> (Graph, Node) {
+    let mut builder = NetworkBuilder::default();
 
-    // inputs
-    let stm = builder.new_sparse_input("stm", Shape::new(num_inputs, 1), max_active);
-    let nstm = builder.new_sparse_input("nstm", Shape::new(num_inputs, 1), max_active);
+    let stm = builder.new_sparse_input("stm", Shape::new(INPUTS, 1), 32);
+    let nstm = builder.new_sparse_input("nstm", Shape::new(INPUTS, 1), 32);
     let targets = builder.new_dense_input("targets", Shape::new(1, 1));
-    let buckets = builder.new_sparse_input("buckets", Shape::new(num_buckets, 1), 1);
 
-    // trainable weights
-    let l0 = builder.new_affine("l0", num_inputs, hl);
-    let l1 = builder.new_affine("l1", hl, num_buckets * 16);
-    let l2 = builder.new_affine("l2", 30, num_buckets * 32);
-    let l3 = builder.new_affine("l3", 32, num_buckets);
-    let pst = builder.new_weights("pst", Shape::new(1, num_inputs), InitSettings::Zeroed);
+    let l0 = builder.new_affine("l0", INPUTS, hl);
+    let l1 = builder.new_affine("l1", 2 * hl, 1);
 
-    // inference
     let stm_subnet = l0.forward(stm).crelu();
     let ntm_subnet = l0.forward(nstm).crelu();
-    let mut out = stm_subnet.concat(ntm_subnet);
-
-    out = out.pairwise_mul_post_affine_dual();
-    out = l1.forward(out).select(buckets);
-
-    let skip_neuron = out.slice_rows(15, 16);
-    out = out.slice_rows(0, 15);
-
-    out = out.concat(out.abs_pow(2.0));
-    out = out.crelu();
-
-    out = l2.forward(out).select(buckets).screlu();
-    out = l3.forward(out).select(buckets);
-
-    let pst_out = pst.matmul(stm) - pst.matmul(nstm);
-    out = out + skip_neuron + pst_out;
-
+    let out = l1.forward(stm_subnet.concat(ntm_subnet));
     let pred = out.sigmoid();
     pred.squared_error(targets);
 
-    // graph, output node
     let output_node = out.node();
+    (builder.build(ExecutionContext::default()), output_node)
+}
+
+#[allow(unused)]
+fn build_lora(hl: usize, rank: usize) -> (Graph, Node) {
+    let mut builder = NetworkBuilder::default();
+
+    let stm = builder.new_sparse_input("stm", Shape::new(INPUTS, 1), 32);
+    let nstm = builder.new_sparse_input("nstm", Shape::new(INPUTS, 1), 32);
+    let targets = builder.new_dense_input("targets", Shape::new(1, 1));
+
+    let l0x = builder.new_weights("l0x", Shape::new(hl, rank), InitSettings::Normal { mean: 0.0, stdev: 0.1 });
+    let l0y = builder.new_weights("l0y", Shape::new(rank, INPUTS), InitSettings::Normal { mean: 0.0, stdev: 0.1 });
+    let l0b = builder.new_weights("l0b", Shape::new(hl, 1), InitSettings::Zeroed);
+    let l1 = builder.new_affine("l1", 2 * hl, 1);
+
+    let l0 = l0x.matmul(l0y);
+    let stm_subnet = (l0.matmul(stm) + l0b).crelu();
+    let ntm_subnet = (l0.matmul(nstm) + l0b).crelu();
+    let out = l1.forward(stm_subnet.concat(ntm_subnet));
+    let pred = out.sigmoid();
+    pred.squared_error(targets);
+
+    let output_node = out.node();
+    builder.set_compile_args(GraphCompileArgs::default().emit_ir());
     (builder.build(ExecutionContext::default()), output_node)
 }
