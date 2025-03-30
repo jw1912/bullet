18c3ab1e36d919fa68007a3e9c0a585a4eb98984
Jamie Whiting <99771266+jw1912@users.noreply.github.com>
2025-03-30 15:08:37 +0100

    Add MNIST example (#347)

---
+++ examples/extra/mnist.rs
@@ -0,0 +1,188 @@
+/// Simple architecture to fit to the MNIST dataset (download from https://www.kaggle.com/datasets/hojjatk/mnist-dataset/)
+/// Move the following four files into data/mnist and run with `cargo r -r --example mnist`
+/// - train-images.idx3-ubyte
+/// - train-labels.idx1-ubyte
+/// - t10k-images.idx3-ubyte
+/// - t10k-labels.idx1-ubyte
+use std::{fs::File, io::Read, time::Instant};
+
+use bullet_core::{
+    backend::device::OperationError,
+    graph::{
+        builder::{GraphBuilder, Shape},
+        Graph, Node,
+    },
+};
+use bullet_hip_backend::{DeviceError, ExecutionContext};
+
+fn main() -> Result<(), OperationError<DeviceError>> {
+    let images = Images::new("data/mnist/train-images.idx3-ubyte");
+    let labels = Labels::new("data/mnist/train-labels.idx1-ubyte");
+    let batch_size = labels.vals.len() / 10;
+
+    let validation_images = Images::new("data/mnist/t10k-images.idx3-ubyte");
+    let validation_labels = Labels::new("data/mnist/t10k-labels.idx1-ubyte");
+    let validation_batch_size = validation_labels.vals.len() / 10;
+
+    assert_eq!(batch_size, images.batch_size());
+    assert_eq!(validation_batch_size, validation_images.batch_size());
+
+    let builder = GraphBuilder::default();
+
+    let inputs = builder.new_dense_input("inputs", Shape::new(images.rows, images.cols));
+    let targets = builder.new_dense_input("targets", Shape::new(10, 1));
+
+    let l0 = builder.new_affine("l0", 28 * 28, 128);
+    let l1 = builder.new_affine("l1", 128, 128);
+    let l2 = builder.new_affine("l2", 128, 10);
+
+    let f0 = l0.forward(inputs.reshape(Shape::new(28 * 28, 1))).sigmoid();
+    let f1 = l1.forward(f0).sigmoid();
+    let f2 = l2.forward(f1);
+    f2.softmax_crossentropy_loss(targets);
+
+    let outputs = f2.node();
+    let mut graph = builder.build(ExecutionContext::default());
+
+    graph.get_input_mut("inputs").load_dense_from_slice(Some(batch_size), &images.vals)?;
+    graph.get_input_mut("targets").load_dense_from_slice(Some(batch_size), &labels.vals)?;
+
+    let t = Instant::now();
+    let lr = 0.0001;
+
+    for epoch in 1..=1000 {
+        graph.zero_grads()?;
+        graph.forward()?;
+        graph.backward()?;
+
+        if epoch % 10 == 0 {
+            let valid_acc = calculate_accuracy(&mut graph, outputs, &validation_images, &validation_labels)?;
+            let train_acc = calculate_accuracy(&mut graph, outputs, &images, &labels)?;
+
+            println!(
+                "epoch {epoch} train accuracy {train_acc:.2}% validation accuarcy {valid_acc:.2}% time {:.3}s",
+                t.elapsed().as_secs_f32()
+            );
+
+            graph.get_input_mut("inputs").load_dense_from_slice(Some(batch_size), &images.vals)?;
+            graph.get_input_mut("targets").load_dense_from_slice(Some(batch_size), &labels.vals)?;
+        }
+
+        for id in &graph.weight_ids() {
+            let weight = &mut *graph.get_weights_mut(id);
+
+            if let Some(grd) = &weight.gradients {
+                weight.values.dense_mut()?.add(-lr, grd)?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn calculate_accuracy(
+    graph: &mut Graph<ExecutionContext>,
+    output_node: Node,
+    images: &Images,
+    labels: &Labels,
+) -> Result<f32, OperationError<DeviceError>> {
+    let batch_size = images.batch_size();
+    graph.get_input_mut("inputs").load_dense_from_slice(Some(batch_size), &images.vals)?;
+    graph.get_input_mut("targets").load_dense_from_slice(Some(batch_size), &labels.vals)?;
+    let _ = graph.forward()?;
+
+    let vals = graph.get_node(output_node).get_dense_vals()?;
+    let mut correct = 0;
+    for (predicted, &expected) in vals.chunks(10).zip(labels.indices.iter()) {
+        let mut max = f32::NEG_INFINITY;
+        let mut best = 110;
+
+        for (i, &x) in predicted.iter().enumerate() {
+            if x > max {
+                max = x;
+                best = i;
+            }
+        }
+
+        assert!(best < 10);
+
+        if best == expected.into() {
+            correct += 1;
+        }
+    }
+
+    assert_eq!(batch_size, labels.indices.len());
+
+    Ok(100.0 * correct as f32 / batch_size as f32)
+}
+
+struct Images {
+    vals: Vec<f32>,
+    rows: usize,
+    cols: usize,
+}
+
+impl Images {
+    pub fn new(file: &str) -> Self {
+        let mut reader = File::open(file).unwrap();
+
+        let mut buf = [0u8; 4];
+        reader.read_exact(&mut buf).unwrap();
+        let magic = u32::from_be_bytes(buf);
+        reader.read_exact(&mut buf).unwrap();
+        let size = u32::from_be_bytes(buf);
+        reader.read_exact(&mut buf).unwrap();
+        let rows = u32::from_be_bytes(buf);
+        reader.read_exact(&mut buf).unwrap();
+        let cols = u32::from_be_bytes(buf);
+
+        assert_eq!(magic, 2051);
+
+        let mut bytes = Vec::new();
+        let num_bytes = reader.read_to_end(&mut bytes).unwrap();
+
+        assert_eq!(size * rows * cols, num_bytes as u32);
+
+        Self {
+            vals: bytes.iter().map(|&x| f32::from(x) / f32::from(u8::MAX)).collect(),
+            rows: rows as usize,
+            cols: cols as usize,
+        }
+    }
+
+    pub fn batch_size(&self) -> usize {
+        self.vals.len() / (self.rows * self.cols)
+    }
+}
+
+struct Labels {
+    vals: Vec<f32>,
+    indices: Vec<u8>,
+}
+
+impl Labels {
+    pub fn new(file: &str) -> Self {
+        let mut reader = File::open(file).unwrap();
+
+        let mut buf = [0u8; 4];
+        reader.read_exact(&mut buf).unwrap();
+        let magic = u32::from_be_bytes(buf);
+        reader.read_exact(&mut buf).unwrap();
+        let size = u32::from_be_bytes(buf);
+
+        assert_eq!(magic, 2049);
+
+        let mut bytes = Vec::new();
+        let num_bytes = reader.read_to_end(&mut bytes).unwrap();
+
+        assert_eq!(size, num_bytes as u32);
+
+        let mut vals = vec![0.0; 10 * size as usize];
+
+        for (i, &byte) in bytes.iter().enumerate() {
+            vals[10 * i + usize::from(byte)] = 1.0;
+        }
+
+        Self { vals, indices: bytes }
+    }
+}
