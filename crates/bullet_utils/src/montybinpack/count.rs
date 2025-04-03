89fcf12be3e3ce031d28fb279ee9c3b59c780bf4
jw1912 <jamie.whiting.dev@gmail.com>
2025-04-03 16:23:33 +0100

    format

--- crates/bullet_utils/src/montybinpack/count.rs
+++ crates/bullet_utils/src/montybinpack/count.rs
@@ -1,8 +1,4 @@
-use std::{
-    fs::File,
-    io::BufReader,
-    path::PathBuf,
-};
+use std::{fs::File, io::BufReader, path::PathBuf};
 
 use montyformat::MontyValueFormat;
 use structopt::StructOpt;
