18bd1675abb0f129e02440cdd7b495367c6a5841
jw1912 <jamie.whiting.dev@gmail.com>
2025-04-03 16:23:10 +0100

    Add some more montybinpack utils

--- crates/bullet_utils/src/montybinpack.rs
+++ crates/bullet_utils/src/montybinpack.rs
@@ -1,16 +1,22 @@
+mod count;
+mod head;
 mod interleave;
 
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
 pub enum MontyBinpackOptions {
+    Head(head::HeadOptions),
     Interleave(interleave::InterleaveOptions),
+    Count(count::CountOptions),
 }
 
 impl MontyBinpackOptions {
     pub fn run(&self) -> anyhow::Result<()> {
         match self {
             Self::Interleave(options) => options.run(),
+            Self::Head(options) => options.run(),
+            Self::Count(options) => options.run(),
         }
     }
 }
