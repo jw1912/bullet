9c2c91cbb3cd44a1ad1f0cb1c1a28ac4d42877ec
Jamie Whiting <99771266+jw1912@users.noreply.github.com>
2025-05-08 17:52:18 +0100

    Verify merged kings bucket layouts (#385)

--- crates/bullet_lib/src/game/inputs/chess_buckets_mk.rs
+++ crates/bullet_lib/src/game/inputs/chess_buckets_mk.rs
@@ -10,6 +10,18 @@ pub struct ChessBucketsMergedKings {
 
 impl ChessBucketsMergedKings {
     pub fn new(buckets: [usize; 64]) -> Self {
+        for (sq, bucket) in buckets.iter().enumerate() {
+            for (sq2, bucket2) in buckets.iter().enumerate() {
+                if bucket == bucket2 {
+                    let rank_diff = (sq / 8).abs_diff(sq2 / 8);
+                    let file_diff = (sq % 8).abs_diff(sq2 % 8);
+                    if rank_diff > 1 || file_diff > 1 {
+                        panic!("Invalid bucket layout in ChessBucketsMergedKings!");
+                    }
+                }
+            }
+        }
+
         Self { buckets, num_buckets: get_num_buckets(&buckets) }
     }
 }
@@ -57,12 +69,24 @@ pub struct ChessBucketsMergedKingsMirrored {
 
 impl ChessBucketsMergedKingsMirrored {
     pub fn new(buckets: [usize; 32]) -> Self {
+        for (sq, bucket) in buckets.iter().enumerate() {
+            for (sq2, bucket2) in buckets.iter().enumerate() {
+                if bucket == bucket2 {
+                    let rank_diff = (sq / 4).abs_diff(sq2 / 4);
+                    let file_diff = (sq % 4).abs_diff(sq2 % 4);
+                    if rank_diff > 1 || file_diff > 1 {
+                        panic!("Invalid bucket layout in ChessBucketsMergedKingsMirrored!");
+                    }
+                }
+            }
+        }
+
         let mut expanded = [0; 64];
         for (idx, elem) in expanded.iter_mut().enumerate() {
             *elem = buckets[(idx / 8) * 4 + [0, 1, 2, 3, 3, 2, 1, 0][idx % 8]];
         }
 
-        Self { wrapped: ChessBucketsMergedKings::new(expanded) }
+        Self { wrapped: ChessBucketsMergedKings { buckets: expanded, num_buckets: get_num_buckets(&expanded) } }
     }
 }
 
