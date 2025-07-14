3bd27ae7a9ae262b97af6582d3fabdfbf8a1a4ed
jw1912 <jamie.whiting.dev@gmail.com>
2025-07-14 19:36:55 +0100

    add new tests

--- examples/tests/test1.rs
+++ examples/tests/test1.rs
@@ -1,11 +1,7 @@
-/*
-This is used to confirm non-functional changes for bullet.
-*/
 use bullet_lib::{
     game::inputs::Chess768,
     nn::optimiser::AdamW,
     trainer::{
-        save::SavedFormat,
         schedule::{lr, wdl, TrainingSchedule, TrainingSteps},
         settings::LocalSettings,
     },
@@ -17,12 +13,7 @@ fn main() {
         .dual_perspective()
         .optimiser(AdamW)
         .inputs(Chess768)
-        .save_format(&[
-            SavedFormat::id("l0w").quantise::<i16>(255),
-            SavedFormat::id("l0b").quantise::<i16>(255),
-            SavedFormat::id("l1w").quantise::<i16>(64),
-            SavedFormat::id("l1b").quantise::<i16>(255 * 64),
-        ])
+        .save_format(&[])
         .loss_fn(|output, target| output.sigmoid().squared_error(target))
         .build(|builder, stm_inputs, ntm_inputs| {
             // weights
@@ -36,15 +27,10 @@ fn main() {
             l1.forward(hidden_layer)
         });
 
-    println!("Single Step:");
-    trainer.optimiser.graph.display("zero_grads").unwrap();
-    trainer.optimiser.graph.display("forward").unwrap();
-    trainer.optimiser.graph.display("backward").unwrap();
-
-    trainer.load_from_checkpoint("checkpoints/testnet");
+    trainer.optimiser.load_weights_from_file("examples/tests/checkpoints/test1.wgts").unwrap();
 
     let schedule = TrainingSchedule {
-        net_id: "testnet".to_string(),
+        net_id: "test1".to_string(),
         eval_scale: 400.0,
         steps: TrainingSteps { batch_size: 16_384, batches_per_superbatch: 1, start_superbatch: 1, end_superbatch: 10 },
         wdl_scheduler: wdl::ConstantWDL { value: 0.2 },
@@ -54,7 +40,7 @@ fn main() {
 
     let settings = LocalSettings { threads: 4, test_set: None, output_directory: "checkpoints", batch_queue_size: 512 };
 
-    let data_loader = DirectSequentialDataLoader::new(&["data/batch1.data"]);
+    let data_loader = DirectSequentialDataLoader::new(&["examples/tests/batch.bf"]);
 
     trainer.run(&schedule, &settings, &data_loader);
 
