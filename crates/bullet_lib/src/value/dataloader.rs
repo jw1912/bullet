fd7204bf06974c940a2b7314096ac775902de613
jw1912 <jamie.whiting.dev@gmail.com>
2025-07-17 17:23:19 +0100

    Pass batch size in `Dataloader::map_batches`

--- crates/bullet_lib/src/value/dataloader.rs
+++ crates/bullet_lib/src/value/dataloader.rs
@@ -34,14 +34,20 @@ where
 {
     type Error = DataLoadingError;
 
-    fn map_batches<F: FnMut(PreparedBatchHost) -> bool>(self, mut f: F) -> Result<(), DataLoadingError> {
+    fn map_batches<F: FnMut(PreparedBatchHost) -> bool>(
+        self,
+        batch_size: usize,
+        mut f: F,
+    ) -> Result<(), DataLoadingError> {
         let ValueDataLoader { dataloader, steps, threads, wdl } = self;
         let start_batch = steps.batches_per_superbatch * (steps.start_superbatch - 1);
+
+        assert_eq!(batch_size, steps.batch_size);
 
         let mut batch_no = 0;
         let mut superbatch = 1;
 
-        dataloader.load_and_map_batches(start_batch, steps.batch_size, |batch| {
+        dataloader.load_and_map_batches(start_batch, batch_size, |batch| {
             let blend = wdl.blend(batch_no, superbatch, steps.end_superbatch);
             let prepared_data = dataloader.prepare(batch, threads, blend);
 
