19997b4f58c7c348914123c75a9e7ce262d4f418
jw1912 <jamie.whiting.dev@gmail.com>
2025-07-21 13:46:25 +0100

    hm

--- crates/bullet_hip_backend/kernels/sparse/bwd.cu
+++ crates/bullet_hip_backend/kernels/sparse/bwd.cu
@@ -1,5 +1,62 @@
 #include "../util.cu"
 
+#if __CUDA_ARCH__ >= 900
+template<OpType op>
+__global__ void sparse_affine_backward_kernel_aligned(
+    const int32_t stride,
+    const int32_t nnz,
+    const int32_t m,
+    const int32_t k,
+    const bool Bb,
+    const int32_t* X,
+    const float* V,
+    const float4* Y,
+    const float4* Yg,
+    float4* Ag,
+    float4* Bg)
+{
+    const int32_t loc = maximumBlocks * blockIdx.z + blockIdx.y;
+    const int32_t row = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (4 * row >= m || loc >= k)
+        return;
+
+    const int32_t* tX = X + nnz * loc;
+    const int32_t offset = stride * m * loc / 4;
+
+    const float4 tY = Y[offset + row];
+    float4 tE = Yg[offset + row];
+
+    tE.x *= op(tY.x);
+    tE.y *= op(tY.y);
+    tE.z *= op(tY.z);
+    tE.w *= op(tY.w);
+
+    if (Bg != nullptr)
+    {   
+        const int32_t offset2 = Bb ? m * loc / 4 : 0;
+        atomicAdd(Bg + offset2 + row, tE);
+    }
+
+    for (int32_t i = 0; i < nnz; i++)
+    {
+        const int32_t j = tX[i];
+        const float val = V == nullptr ? 1.0 : V[nnz * loc + i];
+
+        if (j == -1)
+            break;
+
+        float4 ttE = tE;
+        ttE.x *= val;
+        ttE.y *= val;
+        ttE.z *= val;
+        ttE.w *= val;
+
+        atomicAdd(Ag + j * m / 4 + row, ttE);
+    }
+}
+#endif
+
 template<OpType op>
 __global__ void sparse_affine_backward_kernel(
     const int32_t stride,
@@ -40,7 +97,7 @@ __global__ void sparse_affine_backward_kernel(
             break;
 
         if (tE != 0.0F)
-            atomicAdd(&Ag[j * m + row], val * tE);
+            atomicAdd(Ag + j * m + row, val * tE);
     }
 }
 
@@ -58,14 +115,48 @@ void sparse_affine_backward_internal(
     float* Ag,
     float* Bg)
 {
-    const int32_t chunks = (m + 1023) / 1024;
-    const int32_t threads = (chunks == 1) ? m : 1024;
+    
 
-    int32_t ky = min(k, maximumBlocks);
-    int32_t kz = (k + maximumBlocks - 1) / maximumBlocks;
-    dim3 grid(chunks, ky, kz);
+#if __CUDA_ARCH__ >= 900
+    if ((m % 4) == 0 && m >= 128)
+    {
+        const int32_t max_threads = 1024;
+        const int32_t m4_size = (m + 3) / 4; 
+        const int32_t threads = min(m4_size, max_threads);
+        const int32_t chunks = (m4_size + threads - 1) / threads;
 
-    sparse_affine_backward_kernel<op><<<grid, threads>>>(stride, nnz, m, k, Bb, X, V, Y, Yg, Ag, Bg);
+        int32_t ky = min(k, maximumBlocks);
+        int32_t kz = (k + maximumBlocks - 1) / maximumBlocks;
+        dim3 grid(chunks, ky, kz);
+
+        sparse_affine_backward_kernel_aligned<op><<<grid, threads, alloc>>>(
+            stride,
+            nnz,
+            m,
+            k,
+            Bb, 
+            X,
+            V,
+            reinterpret_cast<const float4*>(Y),
+            reinterpret_cast<const float4*>(Yg),
+            reinterpret_cast<float4*>(Ag),
+            reinterpret_cast<float4*>(Bg)
+        );
+    }
+    else
+    {
+#endif
+        const int32_t chunks = (m + 1023) / 1024;
+        const int32_t threads = (chunks == 1) ? m : 1024;
+
+        int32_t ky = min(k, maximumBlocks);
+        int32_t kz = (k + maximumBlocks - 1) / maximumBlocks;
+        dim3 grid(chunks, ky, kz);
+
+        sparse_affine_backward_kernel<op><<<grid, threads>>>(stride, nnz, m, k, Bb, X, V, Y, Yg, Ag, Bg);
+#if __CUDA_ARCH__ >= 900
+    }
+#endif
 }
 
 extern "C" void sparse_affine_backward(
