596944ab596099a728449399f0ef7520c6570b8d
Jamie Whiting <99771266+jw1912@users.noreply.github.com>
2025-08-01 22:50:25 +0100

    Fix typo in graphviz output (#431)

--- crates/bullet_core/src/graph/ir.rs
+++ crates/bullet_core/src/graph/ir.rs
@@ -318,14 +318,14 @@ where
 
         if let Some(path) = self.opts.dump_graphviz.clone() {
             use std::io::Write;
-            let opts = "label=\"Unoptimised\";style=filled;color=lightgrey;node [style=filled,color=white];";
+            let opts = "style=filled;color=lightgrey;node [style=filled,color=white];";
             let unoptim = self.as_graphviz("unoptim").unwrap();
-            let unoptim = format!("subgraph cluster_0 {{ {opts}{unoptim} }}");
+            let unoptim = format!("subgraph cluster_0 {{ label=\"Unoptimised\";{opts}{unoptim} }}");
 
             self.optimise()?;
 
             let optim = self.as_graphviz("optim").unwrap();
-            let optim = format!("subgraph cluster_1 {{ {opts}{optim} }}");
+            let optim = format!("subgraph cluster_1 {{ label=\"Optimised\";{opts}{optim} }}");
 
             let mut file = std::fs::File::create(path).unwrap();
             write!(&mut file, "digraph G {{ {unoptim} {optim} }}").unwrap();
