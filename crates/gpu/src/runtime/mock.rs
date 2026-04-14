7ff92dc58fd0c82314a0fbae085ef066d7b568cd
jw1912 <jamie.whiting.dev@gmail.com>
2026-04-14 17:48:24 +0100

    .

--- crates/gpu/src/runtime/mock.rs
+++ crates/gpu/src/runtime/mock.rs
@@ -11,6 +11,8 @@ pub struct MockGpu;
 
 type MockResult = Result<(), String>;
 
+static MSG: &str = "This is a mock runtime! It can't actually do anything!";
+
 #[allow(unused)]
 impl GpuBindings for MockGpu {
     type Err = String;
@@ -23,87 +25,87 @@ impl GpuBindings for MockGpu {
     type Module = ();
 
     unsafe fn driver_init() -> MockResult {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn device_get(ordinal: c_int) -> MockResult {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn device_props(device: Self::Dev) -> Result<DeviceProps, Self::Err> {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn context_create(device: ()) -> MockResult {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn context_destroy(device: ()) -> MockResult {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn context_set(ctx: ()) -> MockResult {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn context_sync() -> Result<(), Self::Err> {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn context_malloc(bytes: usize) -> Result<Self::Ptr, Self::Err> {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn context_free(dev_ptr: Self::Ptr) -> Result<(), Self::Err> {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn context_memset(dev_ptr: Self::Ptr, bytes: usize, value: u8) -> Result<(), Self::Err> {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn context_memcpy_d2h(dst: *mut c_void, src: Self::Ptr, bytes: usize) -> Result<(), Self::Err> {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn context_memcpy_h2d(dst: Self::Ptr, src: *const c_void, bytes: usize) -> Result<(), Self::Err> {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn stream_create() -> MockResult {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn stream_destroy(stream: ()) -> MockResult {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn stream_sync(stream: ()) -> MockResult {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn stream_malloc(stream: (), bytes: usize) -> MockResult {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn stream_free(stream: (), dev_ptr: ()) -> MockResult {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn stream_memset(stream: (), dev_ptr: (), bytes: usize, value: u8) -> MockResult {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn stream_memcpy_d2h(stream: (), dst: *mut c_void, src: (), bytes: usize) -> MockResult {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn stream_memcpy_h2d(stream: (), dst: (), src: *const c_void, bytes: usize) -> MockResult {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn kernel_load(kernel: ()) -> MockResult {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn kernel_launch(
@@ -114,19 +116,19 @@ impl GpuBindings for MockGpu {
         args: *mut *mut c_void,
         smem: c_uint,
     ) -> MockResult {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn module_create(code: *const c_void) -> MockResult {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn module_destroy(module: ()) -> MockResult {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn module_get_kernel(module: (), kernel_name: &CStr) -> MockResult {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn program_compile(
@@ -134,19 +136,19 @@ impl GpuBindings for MockGpu {
         num_options: c_int,
         options: *const *const c_char,
     ) -> Result<Vec<c_char>, String> {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn blas_create() -> Result<Self::BlasHandle, Self::Err> {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn blas_destroy(handle: Self::BlasHandle) -> Result<(), Self::Err> {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn blas_set_stream(handle: Self::BlasHandle, stream: Self::Stream) -> Result<(), Self::Err> {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn blas_gemm(
@@ -156,7 +158,7 @@ impl GpuBindings for MockGpu {
         b: Self::Ptr,
         c: Self::Ptr,
     ) -> Result<(), Self::Err> {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 
     unsafe fn blas_gemm_batched(
@@ -167,6 +169,6 @@ impl GpuBindings for MockGpu {
         b: Self::Ptr,
         c: Self::Ptr,
     ) -> Result<(), Self::Err> {
-        Err("This is a mock runtime! It can't actually do anything!".into())
+        Err(MSG.into())
     }
 }
