eb9f58be728b6842dc961ecaaef668c044701dca
Jonathan Hallström <lmj.hallstrom@gmail.com>
2026-06-15 14:22:01 +0200

    Data loading memory pool (#523)

--- crates/trainer/src/run/dataloader.rs
+++ crates/trainer/src/run/dataloader.rs
@@ -1,4 +1,8 @@
-use std::{collections::BTreeMap, sync::Arc};
+use std::{
+    collections::BTreeMap,
+    mem,
+    sync::{Arc, Mutex},
+};
 
 use bullet_compiler::tensor::TValue;
 use bullet_gpu::{
@@ -26,9 +30,14 @@ pub trait DataLoader: Send + Sync + 'static {
 
 pub struct PreparedBatchHost {
     pub inputs: BTreeMap<String, TValue>,
+    pool: Arc<HostPool>,
 }
 
 impl PreparedBatchHost {
+    pub fn new(pool: Arc<HostPool>, inputs: BTreeMap<String, TValue>) -> Self {
+        Self { inputs, pool }
+    }
+
     pub fn copy_to_device_async<'a, G: Gpu>(
         &'a self,
         stream: &Arc<Stream<G>>,
@@ -49,5 +58,59 @@ impl PreparedBatchHost {
             .iter()
             .map(|(id, value)| Buffer::from_host(device, value).map(|tensor| (id.clone(), tensor)))
             .collect()
+    }
+}
+
+impl Drop for PreparedBatchHost {
+    fn drop(&mut self) {
+        for (_, value) in mem::take(&mut self.inputs) {
+            self.pool.give(value);
+        }
+    }
+}
+
+#[derive(Default)]
+struct Pool<T> {
+    free: Mutex<Vec<T>>,
+}
+
+impl<T> Pool<T> {
+    fn take(&self) -> Option<T> {
+        self.free.lock().unwrap().pop()
+    }
+
+    fn give(&self, value: T) {
+        self.free.lock().unwrap().push(value);
+    }
+}
+
+impl<E: Clone + Default> Pool<Vec<E>> {
+    fn take_vec(&self, len: usize) -> Vec<E> {
+        let mut value = self.take().unwrap_or_default();
+        value.resize(len, E::default());
+        value
+    }
+}
+
+#[derive(Default)]
+pub struct HostPool {
+    i32s: Pool<Vec<i32>>,
+    f32s: Pool<Vec<f32>>,
+}
+
+impl HostPool {
+    pub fn take_i32(&self, len: usize) -> Vec<i32> {
+        self.i32s.take_vec(len)
+    }
+
+    pub fn take_f32(&self, len: usize) -> Vec<f32> {
+        self.f32s.take_vec(len)
+    }
+
+    fn give(&self, value: TValue) {
+        match value {
+            TValue::I32(v) => self.i32s.give(v),
+            TValue::F32(v) => self.f32s.give(v),
+        }
     }
 }
