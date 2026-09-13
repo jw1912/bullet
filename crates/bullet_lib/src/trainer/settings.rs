19bc5a9065eb5bacb449d2bcc78845b7ade6a3af
maxolek <nightdreamer312@gmail.com>
2026-09-13 12:26:29 -0500

    style guides

--- crates/bullet_lib/src/trainer/settings.rs
+++ crates/bullet_lib/src/trainer/settings.rs
@@ -48,7 +48,10 @@ impl LocalSettings<'_> {
 
         if let Some(test) = self.test_set {
             println!("Validation Data        : {}", ansi(test.path, "32;1"));
-            println!("   Frequency           : {}", ansi(if test.freq == 0 {"End of superbatch".to_string()} else {format!("{}", test.freq)}, 31));
+            println!(
+                "   Frequency           : {}", 
+                ansi(if test.freq == 0 { "End of superbatch".to_string() } else { format!("{}", test.freq) }, 31)
+            );
             println!("   Batches             : {}", ansi(test.batches, 31));
         }
     }
